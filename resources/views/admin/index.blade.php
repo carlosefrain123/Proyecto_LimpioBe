dbcd206d4ff9ca36e844ab618825336f3bd949b1
carlosefrain123 <carlosefrainchallo1@gmail.com>
2024-01-21 13:38:13 -0500

    21/01/2024 - 13:

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -17,7 +17,54 @@
 @stop
 
 @section('content')
+    <div class="text-center">
+        <img src="{{Auth::user()->photo ? asset('storage/'.Auth::user()->photo):asset('img/sfpr.jpg')}}" width="250"  height="250" class="rounded-circle">
+    </div>
+    <br>
     <h5 class="text-center">¡Hola! <b>{{Auth::user()->name}}</b> desde aquí podras administrar</h5>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header text-center">
+                        <b>Gestionar horario</b>
+                    </div>
+                    <div class="card-body">
+                        <div class="text-center">
+                            <img src="{{asset('img/panel.png')}}" width="50" height="50" alt="">
+                            <a href="#" class="btn btn-primary">Ir al panel</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header text-center">
+                        <b>Gestionar Productos</b>
+                    </div>
+                    <div class="card-body">
+                        <div class="text-center">
+                            <img src="{{asset('img/citas.png')}}" width="50" height="50" alt="">
+                            <a href="#" class="btn btn-primary">Ir a citas</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header text-center">
+                        <b>Gestionar Control</b>
+                    </div>
+                    <div class="card-body">
+                        <div class="text-center">
+                            <img src="{{asset('img/control.png')}}" width="50" height="50" alt="">
+                            <a href="#" class="btn btn-primary">Ir a control</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
 
 @section('css')
