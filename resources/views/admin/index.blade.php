394933639c0453ac0effa59244728b9e5915d9d8
carlosefrain123 <carlosefrainchallo1@gmail.com>
2024-01-21 13:10:50 -0500

    21/01/2024 - 13:10 - Se agrega en el encambezado el logo de la empresa

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,6 +3,16 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <h1 class="text-left"><b>Limpio Be</b> <img src="{{asset('img/logo.png')}}" width="50" height="50" alt=""></h1>
+        </div>
+        <div class="col-6">
+            <h1 class="text-right"><b>{{now()->format('d/m/y')}}</b></h1>
+        </div>
+    </div>
+</div>
     <h1 class="text-center"><b>Limpio Be</b></h1>
 @stop
 
